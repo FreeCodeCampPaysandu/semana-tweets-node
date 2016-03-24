e14ae454a46eb4648854b05aab2daf8f341cde1b
Nicolas Zuasti <nicozuasti@gmail.com>
2016-03-24 11:41:47 -0300

    diferentes filtros

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var Q = require('q');
 
 var T = new Twit({
   consumer_key: 'CZilMOQ8YKcGMzlvElWPN2E4q',
@@ -20,19 +21,56 @@ app.get('/', function (req, res) {
 });
 
 app.get('/initial', function (req, res) {
-  T.get('search/tweets', { q: '#SemanadelaCerveza', count: 200 }, function (err, data, response) {
-    res.json(data.statuses);
+
+  var a = T.get('search/tweets', { q: '#51Semanadelacerveza', count: 200 }, function (err, data, response) {
+    return data.statuses;
   });
+
+  var b = T.get('search/tweets', { q: '#SemanadelaCerveza', count: 200 }, function (err, data, response) {
+    return data.statuses;
+  });
+
+  var c = T.get('search/tweets', { q: '@Semana_Cerveza', count: 200 }, function (err, data, response) {
+    return data.statuses;
+  });
+  eventualAdd(a,b,c);
+
+  function eventualAdd(a, b, c) {
+    return Q.spread([a, b, c], function (a, b, c) {
+
+        var list = a.data.statuses.concat(b.data.statuses);
+        list = list.concat(c.data.statuses);
+        res.json(list);
+    })
+  }
 })
 
 io.on('connection', function (socket) {
   console.log('a user connected');
-  T.get('search/tweets', { q: '#SemanadelaCerveza', count: 200 }, function (err, data, response) {
-    io.emit('initialTweets', data.statuses)
+
+  var a = T.get('search/tweets', { q: '#51Semanadelacerveza', count: 200 }, function (err, data, response) {
+    return data.statuses;
   });
+
+  var b = T.get('search/tweets', { q: '#SemanadelaCerveza', count: 200 }, function (err, data, response) {
+    return data.statuses;
+  });
+
+  var c = T.get('search/tweets', { q: '@Semana_Cerveza', count: 200 }, function (err, data, response) {
+    return data.statuses;
+  });
+  eventualAdd(a,b,c);
+
+  function eventualAdd(a, b, c) {
+    return Q.spread([a, b, c], function (a, b, c) {
+        var list = a.data.statuses.concat(b.data.statuses);
+        list = list.concat(c.data.statuses);
+        io.emit('initialTweets', list);
+    })
+  }
 });
 
-var stream = T.stream('statuses/filter', { track: '#SemanadelaCerveza' });
+var stream = T.stream('statuses/filter', { track: ['#SemanadelaCerveza', '#51Semanadelacerveza', '@Semana_Cerveza'] });
 
 stream.on('tweet', function (tweet) {
   io.emit('tweet', tweet);
